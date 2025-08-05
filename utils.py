55a382b02b5ed579b6e8e46166005164a3f8466d
fsynotfound <shuyaofan.coder@gmail.com>
2025-08-05 22:17:41 +0100

    feat(utils): add email extraction function using regex

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import re
 
-# 用正则表达式从普通文本中找出所有形如 xxx@xxx.com 的邮箱，供 scraper.py 调用
+# Extract all email addresses from a given text using a regular expression
 def extract_emails(text):
     pattern = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
     return re.findall(pattern, text)
